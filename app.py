923c1b6868bc1e68de2905125db0161e19b41ef5
Weining Mai <84426364+weibb123@users.noreply.github.com>
2024-07-25 15:06:52 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from openai import OpenAI
+from langchain_experimental.llms.ollama_functions import OllamaFunctions
+
+model = OllamaFunctions(model="llama3.1")
+
+st.title("Local-Hosted LLM")
+st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+
+    client = OpenAI(api_key="ollama", base_url="http://localhost:11434/v1")
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="llama3.1", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
